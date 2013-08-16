cad34afbc6a9653077b7e159524e28faa5f4045b
Michael Ilyin <milyin@gmail.com>
2013-08-16 18:02:18 +0400

    Implemented serving 1x1 image in by '/tr' link. Implemented correct indexing visit stats

--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,9 @@
 
 module Main where
 
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C
+import qualified Data.ByteString.Lazy.Char8 as L
 import Happstack.Server
 import qualified DStatus
 import DStatus (measure, DStatus)
@@ -18,6 +21,16 @@ import Data.Acid.Advanced
 import Data.Acid.Local
 import Data.List
 import Stat
+
+data PointImg = PointImg
+instance ToMessage PointImg where
+    toContentType _ = C.pack "image/gif"
+    toMessage _ = L.pack 
+        "\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\
+        \\x80\x00\x00\xFF\xFF\xFF\x00\x00\x00\x21\
+        \\xF9\x04\x01\x00\x00\x00\x00\x2C\x00\x00\
+        \\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\
+        \\x01\x00\x3B"
 
 -- TODO - make file config
 newConf dvar = Conf { 
@@ -39,6 +52,14 @@ countAction acidStats = do
     c1::Int <- update' acidStats IncStatsCount
     ok $ toResponse $ H.toHtml $ concat $ intersperse "," $ map show [c0, c1]
 
+trAction :: AcidState Stats -> ServerPart Response
+trAction acidStats = do
+--    headers <- rqHeaders `liftM` askRq
+--    lift $ putStrLn $ show headers
+    referer <- (getHeader "referer") `liftM` askRq
+    lift $ putStrLn $ show referer
+    ok $ toResponse PointImg
+
 main = do
     dvar <- DStatus.new
     let conf = newConf dvar
@@ -46,8 +67,11 @@ main = do
         (openLocalState initialStats)
         (createCheckpointAndClose)
         (\acidStats -> simpleHTTP conf $ msum [
-            dir "dstatus" $ DStatus.measure dvar $ dstatusAction dvar,
-            dir "count"   $ DStatus.measure dvar $ countAction acidStats
+            dir "dstatus"     $ measure dvar $ dstatusAction dvar,
+            dir "count"       $ measure dvar $ countAction acidStats,
+            dir "tr"          $ measure dvar $ trAction acidStats,
+            dir "favicon.ico" $ measure dvar $ serveFile (asContentType "image/vnd.microsoft.icon") "favicon.ico",
+            dir "static"      $ measure dvar $ serveDirectory EnableBrowsing [] "html" 
         ])
 
 
--- src/Stat.hs
+++ src/Stat.hs
@@ -16,12 +16,15 @@ import Control.Monad
 import Control.Monad.State ( get, put )
 import Control.Monad.Reader ( ask )
 import Control.Applicative ( (<$>) )
-import Data.Lens
-import Data.Lens.Template
-import Data.Lens.IxSet
+import Data.Lens.Common
+import Data.Lens.Template (makeLens)
+import Data.Lens.IxSet (ixLens)
 
 newtype Url = Url B.ByteString  deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy)
 newtype DayHour = DayHour Int8 deriving (Eq, Ord, Read, Show, Data, Typeable, SafeCopy)
+
+localDayHour :: LocalTime -> DayHour
+localDayHour localTime = DayHour $ fromIntegral $ todHour $ localTimeOfDay localTime
 
 newtype Region = Region String
     deriving (Eq, Ord, Read, Show, Data, Typeable)
@@ -32,34 +35,56 @@ data Visit = Visit {
     visitUrl  :: Url
     }
 
-data Stat = Stat {
+data StatIndex = StatIndex {
     _statUrl    :: Url,
-    _statDate   :: Day,
+    _statDay    :: Day,
     _statHour   :: DayHour,
-    _statRegion :: Region,
-    _statCount  :: Int32
+    _statRegion :: Region
+    } deriving (Eq, Ord, Read, Show, Data, Typeable)
+
+$(makeLens ''StatIndex)
+
+visit2index :: TimeZone -> Visit -> StatIndex
+visit2index tz visit = StatIndex {
+    _statUrl    = visitUrl visit,
+    _statDay    = localDay visitLocalTime,
+    _statHour   = localDayHour visitLocalTime,
+    _statRegion = ip2region $ visitIp visit
+    } where
+        visitLocalTime = utcToLocalTime tz $ visitTime visit
+
+ip2region :: (Either IPv4 IPv6) -> Region
+ip2region (Left _) = Region "IPv4"
+ip2region (Right _) = Region "IPv6"
+
+data Stat = Stat {
+    _statIndex  :: StatIndex,
+    _statCount  :: Int
     } deriving (Eq, Ord, Read, Show, Data, Typeable)
 
 $(makeLens ''Stat)
 
 instance Indexable Stat where
     empty = ixSet [ 
-        ixFun' statUrl,
-        ixFun' statDate,
-        ixFun' statHour,
-        ixFun $ \s -> [ (statDate ^$ s, statHour ^$ s) ],
-        ixFun' statRegion
+        ixFun $ \s -> [               statIndex ^$ s ],
+        ixFun $ \s -> [ statUrl    ^$ statIndex ^$ s ],
+        ixFun $ \s -> [ statDay    ^$ statIndex ^$ s ],
+        ixFun $ \s -> [ statHour   ^$ statIndex ^$ s ],
+        ixFun $ \s -> [ statRegion ^$ statIndex ^$ s ],
+        ixFun $ \s -> [ (statDay   ^$ statIndex ^$ s, statHour ^$ statIndex ^$ s) ]
         ]
-        where ixFun' lens = ixFun $ \a -> [ lens ^$ a ] 
 
 data Stats = Stats {
-    _stats :: IxSet Stat,
+    _statsSet :: IxSet Stat,
     _statsCount :: Int
     } deriving (Data, Typeable)
 
 $(makeLens ''Stats)
 
-initialStats = Stats { _stats = empty, _statsCount = 0 }
+stat :: (Typeable key) => key -> Lens (IxSet Stat) (Maybe Stat)
+stat key = ixLens key
+
+initialStats = Stats { _statsSet = empty, _statsCount = 0 }
 
 incStatsCount :: Update Stats Int
 incStatsCount = do
@@ -71,12 +96,17 @@ incStatsCount = do
 peekStatsCount :: Query Stats Int
 peekStatsCount = getL statsCount <$> ask
 
--- incStats :: Visit -> Update Stats Int
--- incStats visit = do
---    s@Stats{..} <- get
+incStats :: StatIndex -> Update Stats Int
+incStats index = do
+    s <- get
+    let rec = maybe (Stat index 1) (statCount ^%= succ) (stat index ^$ statsSet ^$ s)
+    let new_s = (statsSet ^%= updateIx index rec) s
+    put $ new_s
+    return $ statCount ^$ rec
 
-$(makeAcidic ''Stats ['incStatsCount, 'peekStatsCount])
+$(makeAcidic ''Stats ['incStatsCount, 'peekStatsCount, 'incStats])
 $(deriveSafeCopy 0 'base ''Region)
+$(deriveSafeCopy 0 'base ''StatIndex)
 $(deriveSafeCopy 0 'base ''Stat)
 $(deriveSafeCopy 0 'base ''Stats)
 
